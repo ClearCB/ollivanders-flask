c0df8ecd5118bed1862317b293351ae2d2a78f47
Abel Casas <acasasgarcia1@cifpfbmoll.eu>
2023-03-18 10:46:22 +0100

    test(models): add test to check if the models to create a document in mongo works as expected

---
+++ repository/models.py
@@ -0,0 +1,9 @@
+
+class Item():
+
+    def __init__(self, id, name, sell_in, quality):
+
+        self.id == id
+        self.name == name
+        self.sell_in == sell_in
+        self.quality == quality
