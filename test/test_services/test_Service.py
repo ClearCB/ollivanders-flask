64af5c4071e25dc5a34a957e950d389ade105c0b
Abel Casas <acasasgarcia1@cifpfbmoll.eu>
2023-03-18 21:25:13 +0100

    feat(Services): create static class to interact with the database

--- test/test_services/test_Service.py
+++ test/test_services/test_Service.py
@@ -1,53 +1,74 @@
-
+from services.Services import Services
+from repository.Database import Database
 import pytest
+
 
 @pytest.mark.test_create_one
 def test_create_one():
 
-    assert Services.create_one(1,"Sulfutas",1,1,"Sulfuras").inserted_id == 1
+    Services.delete_one(10000)
+    assert (
+        Services.create_one(
+            10000, name="Sulfutas", sell_in=1, quality=1, item_type="Sulfuras"
+        ).inserted_id
+        == 10000
+    )
+    Services.delete_one(10000)
+
 
 @pytest.mark.test_get_one
 def test_get_one():
 
+    Services.create_one(
+        10000, name="Sulfutas", sell_in=1, quality=1, item_type="Sulfuras"
+    )
     item = {
-        "_id":1,
-        "name":"Sulfutas",
-        "sell_in":1,
-        "quality":1,
-        "item_type":"Sulfuras"
-        }
+        "_id": 10000,
+        "name": "Sulfutas",
+        "sell_in": 1,
+        "quality": 1,
+        "item_type": "Sulfuras",
+    }
 
-    assert Services.get_one(1) == item
+    assert Services.get_one(10000) == item
+    Services.delete_one(10000)
+
 
 @pytest.mark.test_get_inventory
 def test_get_inventory():
 
-    Services.create_one(2,"Sulfutas",1,1,"Sulfuras")
+    Services.create_one(
+        10000, name="Sulfutas", sell_in=1, quality=1, item_type="Sulfuras"
+    )
+    assert isinstance(Services.inventory(), list)
+    assert isinstance(Services.inventory()[0], dict)
+    assert Database.correct_item(Services.inventory()[0])
 
-    inventory = [
-        Services.get_one(1),
-        Services.get_one(2)
-        ]
-    
-    assert Services.inventory() == inventory
+    Services.delete_one(10000)
 
 
 @pytest.mark.test_update_one
 def test_update_one():
 
-    assert Services.update_one(1,name="haha").modified_count == 1
+    Services.create_one(
+        10000, name="Sulfutas", sell_in=1, quality=1, item_type="Sulfuras"
+    )
+    assert Services.update_one(10000, name="haha") == 1
 
     item = {
-        "_id":1,
-        "name":"haha",
-        "sell_in":1,
-        "quality":1,
-        "item_type":"Sulfuras"
-        }
-    
-    assert Services.get_one(1) == item
+        "_id": 10000,
+        "name": "haha",
+        "sell_in": 1,
+        "quality": 1,
+        "item_type": "Sulfuras",
+    }
+
+    assert Services.get_one(10000) == item
+
+    Services.delete_one(10000)
+
 
 @pytest.mark.test_delete_one
 def test_delete_one():
 
-    assert Services.create_one(1,"Sulfutas",1,1,"Sulfuras").inserted_id == 1
+    assert Services.delete_one(10000) == 1
