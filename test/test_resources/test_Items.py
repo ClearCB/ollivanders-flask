bb958ddf616bdcc1a48d53e309604ca93b8e39fc
ClearCB <abelcasasccb@gmail.com>
2023-03-21 16:26:05 +0100

    test(CRUD):  create test about the api

--- test/test_resources/test_Items.py
+++ test/test_resources/test_Items.py
@@ -48,9 +48,49 @@ def test_delete_item(client):
     m = {"Item id deleted": "0"}
 
     assert response.json == m
+    assert response.status_code == 200
 
     response = client.delete("/items/delete-one/0")
 
-    error = {"error": "Item not found"}
+    error = {"ERROR": "Item not found"}
 
+    assert response.status_code == 200
     assert response.json == error
+
+
+@pytest.mark.test_update_item
+def test_update_item(client):
+
+    item = {
+        "_id": 0,
+        "name": "test_item",
+        "sell_in": 5,
+        "quality": 10,
+        "item_type": "NormalItem",
+    }
+    update_statement = {
+        "sell_in":8
+    }
+
+    response = client.update(0,update_statement)
+
+    assert response.status_code == 200
+    assert response.json == {"Item id updated":"0"}
+
+@pytest.mark.test_create_item
+def test_create_item(client):
+
+    item = {
+        "_id": 1000000,
+        "name": "test_item",
+        "sell_in": 5,
+        "quality": 10,
+        "item_type": "NormalItem",
+    }
+
+    response = client.post("/items/create-one/",json=item)
+
+
+    assert response.status_code == 200
+    assert response.json == {"_id":1000000}
+
