6d64a7c832cf0d355f69bb30f5199497f95a60e6
Abel Casas <acasasgarcia1@cifpfbmoll.eu>
2023-03-22 15:20:28 +0100

    refactor

--- repository/Database.py
+++ repository/Database.py
@@ -6,12 +6,21 @@ import os
 class Database:
 
     @staticmethod
-    def init_db():
+    def connect(database):
 
         client = MongoClient(os.environ.get("MONGO_ATLAS_URI"))
-        db = client["ollivander_shop"]
-        collection = db["items"]
-        collection.insert_many(items_day_zero)
+        return client[database]
+
+    @staticmethod
+    def collection(collection):
+
+        return Database.connect("ollivander_shop")[collection]
+
+    @staticmethod
+    def init_db():
+
+
+        Database.collection("items").insert_many(items_day_zero)
 
 
     @staticmethod
@@ -24,45 +33,25 @@ class Database:
     @staticmethod
     def read_one(id):
 
-        client = MongoClient(os.environ.get("MONGO_ATLAS_URI"))
-        db = client["ollivander_shop"]
-        collection = db["items"]
-
-        return collection.find_one({"_id":id})
+        Database.collection("items").find_one({"_id":id})
 
     @staticmethod
     def delete_one(id):
 
-        client = MongoClient(os.environ.get("MONGO_ATLAS_URI"))
-        db = client["ollivander_shop"]
-        collection = db["items"]
-
-        return collection.delete_one({"_id":id})
+        Database.collection("items").delete_one({"_id":id})
 
     @staticmethod
     def create_one(item):
 
-        client = MongoClient(os.environ.get("MONGO_ATLAS_URI"))
-        db = client["ollivander_shop"]
-        collection = db["items"]
-
-        return collection.insert_one(item)
+        Database.collection("items").insert_one(item)
 
     @staticmethod
     def update_one(id, update_statement):
 
-        client = MongoClient(os.environ.get("MONGO_ATLAS_URI"))
-        db = client["ollivander_shop"]
-        collection = db["items"]
-
-        return collection.update_one({"_id":id},{"$set":update_statement})
+        Database.collection("items").update_one({"_id":id},{"$set":update_statement})
 
 
     @staticmethod
     def inventory():
 
-        client = MongoClient(os.environ.get("MONGO_ATLAS_URI"))
-        db = client["ollivander_shop"]
-        collection = db["items"]
-
-        return list(collection.find())
+        return list(Database.collection("items").find())
